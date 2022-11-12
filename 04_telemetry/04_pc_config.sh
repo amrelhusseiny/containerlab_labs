090642221b33198a52b9da182d16ffb7f5b82bb7
Amr ElHusseiny <amroashram@hotmail.com>
2022-11-12 04:26:19 +0200

    Lab 04 Updated

---
+++ 04_telemetry/04_pc_config.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo docker exec clab-testing_topology-PC1 ip link set eth1 up
+sudo docker exec clab-testing_topology-PC1 ip addr add 10.0.10.2/24 dev eth1
+sudo docker exec clab-testing_topology-PC1 ip route add 10.0.20.0/24 via 10.0.10.1 dev eth1
+
+sudo docker exec clab-testing_topology-PC2 ip link set eth1 up
+sudo docker exec clab-testing_topology-PC2 ip addr add 10.0.20.2/24 dev eth1
+sudo docker exec clab-testing_topology-PC2 ip route add 10.0.10.0/24 via 10.0.20.1 dev eth1
--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 - Lab1 : a Backbone of IS-IS/MPLS Juniper VMX , Cisco IOS-XR , Nokia SR Linux devices
 - Lab2 : -----------
 - Lab3 : IOS-XR and FRR L3VPN , PEs configures using gNMI and telemetry tested
-- Lab4 : Adding Grafana to gather graph telemetry data 
+- Lab4 : Adding Telegraf (Telemetry Collector) & InfluxDB (Time Series Database)
